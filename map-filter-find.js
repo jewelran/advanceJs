c976f185ccadf1af743b745a91a1b2568cd1faa4
jewelran <mdjewelrana6487@gmail.com>
2021-02-21 17:56:20 +0600

    added

---
+++ map-filter-find.js
@@ -0,0 +1,15 @@
+//map............
+//map array dibe..........
+const number = [12, 32, 53, 23, 64, 35];
+const result = number.map((x) => x * x);
+console.log(result);
+
+//filter.........
+//filter array dibe
+const bigger = number.filter((x) => x > 15);
+console.log(bigger);
+
+// find.....................
+// find shudu element dibe
+const findArray = number.find((x) => x > 50);
+console.log(findArray)

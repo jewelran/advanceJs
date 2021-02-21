f66ee0545ad9285dc410f354a1596cb90c77da76
jewelran <mdjewelrana6487@gmail.com>
2021-02-21 17:29:09 +0600

    added

---
+++ double-vs-three-equal.js
@@ -0,0 +1,30 @@
+const first = 1;
+const second = true;
+if (first === second) {
+    console.log('condition is true')
+}else{
+    console.log("condition is false")
+}
+
+const first = 0;
+const second = true;
+if (first === second) {
+    console.log('condition is true')
+}else{
+    console.log("condition is false")
+}
+
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log('condition is true')
+}else{
+    console.log("condition is false")
+}
+const first = 2;
+const second = "2";
+if (first == second) {
+    console.log('condition is true')
+}else{
+    console.log("condition is false")
+}

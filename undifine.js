56f76411ae247b5f64e9732b5132b27bfc4bbf23
jewelran <mdjewelrana6487@gmail.com>
2021-02-21 17:14:26 +0600

    added

---
+++ undifine.js
@@ -0,0 +1,24 @@
+
+function add(n, n1) {
+  x = n + n1;
+  return 
+}
+const result = add(12, 432);
+console.log(result);
+
+
+function add(n1 , n2){
+    console.log(n1,n2)
+}
+const result = add(12);
+console.log(result);
+
+const premik = {name:"jewel", phone: 12324};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun)
+
+let age = [12, 324, 42,];
+console.log(age[12]);
+
